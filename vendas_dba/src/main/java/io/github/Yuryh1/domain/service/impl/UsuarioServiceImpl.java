31f5e8d8ab5c63bebcd20354300609cfe9714732
Yury Samuel <Yuryh084@gmail.com>
2023-05-10 10:42:31 -0300

    Add files via upload

---
+++ vendas_dba/src/main/java/io/github/Yuryh1/domain/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,42 @@
+package io.github.Yuryh1.domain.service.impl;
+import io.github.Yuryh1.domain.entity.Usuario;
+import io.github.Yuryh1.domain.repositorio.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+
+public class UsuarioServiceImpl implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return repository.save(usuario);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = repository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado na base de dados."));
+
+        String[] roles = usuario.isAdmin() ?
+                new String[]{"ADMIN", "USER"} : new String[]{"USER"};
+
+        return User
+                .builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(roles)
+                .build();
+    }
+}
